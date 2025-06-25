d5453d7fd0590e3af572fe6e01401bde55c210b1
smritisrivatsa <smrits3@uw.edu>
2025-06-24 17:27:57 -0700

    prices

--- 01_preprocessing.py
+++ 01_preprocessing.py
@@ -1,3 +1,16 @@
 # loading the dataset
 # converting price units
-# creating categories
+# creating categories/ price bins
+
+import pandas as pd
+df = pd.read_csv('Mumbai Housing Price.csv')
+
+# convert price to INR
+def convert_price(row):
+    if row['price_unit'] == 'L':
+        return row['price'] * 1e5
+    elif row['price_unit'] == 'Cr':
+        return row['price'] * 1e7
+    return None
+
+df['price_INR'] = df.apply(convert_price, axis = 1)
