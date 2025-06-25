f1ed7ed278f5ef0edd683da5893f21adb67f5dd2
anushkamalpani <anushka.malpani1910@gmail.com>
2025-06-24 17:30:35 -0700

    bin

--- 01_preprocessing.py
+++ 01_preprocessing.py
@@ -1,3 +1,16 @@
 # loading the dataset
+import pandas as pd
+df = pd.read_csv('Mumbai Housing Price.csv')
 # converting price units
+
 # creating categories
+def category(p):
+    if p < 50 * 1e5:
+        return 'Low'
+    elif df.p < 2 * 1e7:
+        return 'Medium'
+    else:
+        return 'High'
+    
+df['price_bin'] = df['price_INR'].apply(category, axis = 1)
+
